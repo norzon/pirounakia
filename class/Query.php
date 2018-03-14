ad01719147545046cad5dfb2da39a0650504a697
Alexandros Leontakianakos <alleon20@gmail.com>
2018-03-14 10:24:45 +0200

    Added comments and renamed function remove to clear in QueryCollection

--- class/Query.php
+++ class/Query.php
@@ -14,16 +14,28 @@
         private $stmt;
 
 
+        /**
+         * Sets the id and the prepared statement
+         */
         public function __construct ($id, $stmt) {
             $this->id = $id;
             $this->stmt = $stmt;
         }
 
 
+        /**
+         * Getter for the id
+         * @return id
+         */
         public function getId () {
             return $this->id;
         }
 
+
+        /**
+         * Returns the prepared statement
+         * @return stmt
+         */
         public function getStmt () {
             return $this->stmt;
         }
--- class/QueryCollection.php
+++ class/QueryCollection.php
@@ -1,28 +1,35 @@
 <?php
+    /**
+     * A collection of key->value pairs, where the key is a unique identifier
+     * and the value is a PDO prepared statement
+     */
     class QueryCollection {
 
         /**
-         * 
+         * An array to store the queries
+         * @access private
          */
         private $collection = [];
 
 
         /**
-         * 
+         * Empty constructor as it is not used
          */
         public function __construct () {}
 
 
         /**
-         * 
+         * Adds a Query object to the collection
+         * @param Query
          */
         public function add (Query $query) {
             $this->collection[$query->getId()] = $query->getStmt();
         }
 
+
         /**
          * Returns a specific query by id
-         * 
+         * @param id If null, returns the collection
          */
         public function get ($id = null) {
             if ($id == null) {
@@ -34,7 +41,11 @@
         }
 
 
-        public function remove ($id) {
+        /**
+         * Removes a query by a specific id, or clears all if no id given
+         * @param id
+         */
+        public function clear ($id) {
             if ($id == null) {
                 $this->collection = [];
             } else if (isset($this->collection[$id])) {
