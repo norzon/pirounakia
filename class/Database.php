83a1bf2e06cc2d1f2df8680f5f20cdf4da439956
Alexandros Leontakianakos <alleon20@gmail.com>
2018-03-20 13:06:41 +0200

    Added getters for users and for one option and added comments

--- class/Database.php
+++ class/Database.php
@@ -63,15 +63,93 @@
 
 
         /**
-         * 
+         * Gets the options from the DB
+         * @access public
          */
         public function getOptions () {
             $this->prepare(
-                "options",
+                "get.options",
                 "SELECT *
                 FROM {$this->tablenames['options']};"
             );
             return $this->execute("options");
+        }
+
+
+        /**
+         * Get option by alias
+         * @access public
+         */
+        public function getOption ($str) {
+            $this->prepare(
+                "get.option",
+                "SELECT *
+                FROM {$this->tablenames['options']}
+                WHERE `alias` LIKE '%{$str}%';"
+            );
+            return $this->execute("options");
+        }
+
+
+        /**
+         * Get all users
+         * @access public
+         */
+        public function getUsers () {
+            $this->prepare(
+                "get.users",
+                "SELECT *
+                FROM {$this->tablenames['user']};"
+            );
+            return $this->execute("get.users");
+        }
+
+
+        /**
+         * Get user by id
+         * @access public
+         * @param id The user's id
+         */
+        public function getUserById ($id) {
+            $this->prepare(
+                "get.user",
+                "SELECT *
+                FROM {$this->tablenames['user']}
+                WHERE `id` = '$id';"
+            );
+            return $this->execute("get.user");
+        }
+
+
+        /**
+         * Get user by email
+         * @access public
+         * @param email The user's email
+         */
+        public function getUserByEmail ($email) {
+            $this->prepare(
+                "get.user",
+                "SELECT *
+                FROM {$this->tablenames['user']}
+                WHERE `email` = '$email';"
+            );
+            return $this->execute("get.user");
+        }
+
+
+        /**
+         * Get user by token
+         * @access public
+         * @param token The user's token
+         */
+        public function getUserByToken ($token) {
+            $this->prepare(
+                "get.user",
+                "SELECT *
+                FROM {$this->tablenames['user']}
+                WHERE `token` = '$token';"
+            );
+            return $this->execute("get.user");
         }
     }
 ?>
